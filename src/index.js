d0313f0f69eef00f7c8f3cb924639499402fcca5
lobana alarabi <loubanaeng@gmail.com>
2020-02-03 07:35:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,5 +12,47 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
 })
+document.addEventListener("DOMContentLoaded", addToys);
+
+function addToys() {
+  return fetch( 'http://localhost:3000/toys', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      let tToy=document.getElementsById('toy-collection');
+      for(let item of object){
+            let newDiv=document.createElement('div');
+            newDiv.className="card";
+            let h=document.createElement("h2");
+            h.innerText=item['Text'];
+            newDiv.appendChild(h);
+            let img=document.createElement("img");
+            img.className="toy-avatar";
+            img.src=item['img'];
+            newDiv.appendChild(img);
+            let p=document.createElement("p");
+            p.innerText=item['p'];
+            newDiv.appendChild(p);
+            let button=document.createElement("button");
+            button.className="like-btn";
+            button.src=item['btn'];
+            newDiv.appendChild(button);
+            tToy.appendChild(newDiv);
+    }
+    } )
+}
+
+let bBtn=document.getElementsById('new-toy-btn');
+bBtn.addEventListener('click',function(){
+  let x=document.getElementsById('')
+}
+
+);
