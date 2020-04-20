70bb91097e22cd1b5c4e21f328f9d63a1a890cd3
eliftabak <elifchorghay@gmail.com>
2020-04-20 20:20:01 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,37 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// let formData = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json"
+//   },
+//   body: JSON.stringify({
+//     "name": name,
+//     "image": image,
+//     "likes": likes
+//   })
+// }
+
+
+function createToy() {
+  fetch('http://localhost:3000/toys')
+  .then((res) => res.json())
+  .then((data) => {
+    let output = '<h2>Toys</h2>'
+    data.forEach(function(toys){
+      output += `
+      <div class="card">
+      <h2>${toys.name}</h2>
+      <img src= ${toys.image} class="toy-avatar" />
+      <p> ${toys.likes} </p>
+      <button class="like-btn">Like <3</button>
+      </div>
+      `;
+      })
+      document.getElementById('toy-collection').innerHTML = output
+  })
+}
+createToy();
